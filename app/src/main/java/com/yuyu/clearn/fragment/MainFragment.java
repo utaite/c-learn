278cb48e74f8c40ba26d13df2024f87cef8a56a8
utaite <yuyu688882@gmail.com>
2016-12-05 10:54:58 +0900

    Utaite
    Update from 16.12.05

--- app/src/main/java/com/yuyu/clearn/fragment/MainFragment.java
+++ app/src/main/java/com/yuyu/clearn/fragment/MainFragment.java
@@ -18,7 +18,6 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_main, container, false);
-
         return view;
     }
 
